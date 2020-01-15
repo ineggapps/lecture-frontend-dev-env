aabfd9ab0ba9411ea3a0688180368e9d186347fd
ineggapps <inegg.apps@gmail.com>
2020-01-15 16:20:48 +0900

    과제 작성 전 (플러그인 삽입까지 완료)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -24,12 +28,36 @@ module.exports = {
         }
       }
     ]
-  }
+  },
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
-   * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
+   * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요. V
    * 2. HtmlTemplatePlugin: 동적으로 html 파일을 생성하세요.
-   * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
-   * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
+   * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요. V
+   * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요. V
    */
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: () => `빌드 날짜: ${new Date().toLocaleString()}`
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // 템플릿 경로를 지정
+      templateParameters: {
+        // 템플리셍 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+      },
+      hash: true,
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
+          : false
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
+  ]
 };
