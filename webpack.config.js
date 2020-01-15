3f2287bedb3dab669c777a9948b8175db4468567
ineggapps <inegg.apps@gmail.com>
2020-01-15 16:07:29 +0900

    파일로더를 불러온 과제 수행

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js"
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
+    filename: "[name].js",
+    path: path.resolve("./dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg)$/, // .png 확장자로 마치는 모든 파일
+        loader: "file-loader",
+        options: {
+          publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
+          name: "[name].[ext]?[hash]" // 파일명 형식
+        }
+      },
+      {
+        test: /\.css$/, // .css 확장자로 끝나는 모든 파일
+        use: ["style-loader", "css-loader"] // css-loader를 적용한다
+      }
+    ]
   }
-}
+};
